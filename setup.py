2d676ce0fd3b347d8d8f9bb9b0704d410cc90564
Yilong Li <yilong.li.yl3@gmail.com>
2019-12-04 21:55:15 -0500

    Move all project code to src
    Following advice from
    https://blog.ionelmc.ro/2014/05/25/python-packaging/#the-structure

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='mutlib',
       version='0.0.0',
       description='Point mutation analysis library',
       author='Yilong Li',
       author_email='yilong.li.yl3@gmail.com',
-      packages=['mutlib'],
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
       zip_safe=False)
