63c517dc542690d72df3ac003b3df9e74dbb7464
Yilong Li <yilong.li.yl3@gmail.com>
2019-12-04 21:39:55 -0500

    Barebones package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='mutlib',
+      version='0.0.0',
+      description='Point mutation analysis library',
+      author='Yilong Li',
+      author_email='yilong.li.yl3@gmail.com',
+      packages=['mutlib'],
+      zip_safe=False)
