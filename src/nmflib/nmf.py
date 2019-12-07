35a2cc5fa9ad86324319a743da5ed2fe8bf25ce4
Yilong Li <yilong.li.yl3@gmail.com>
2019-12-07 16:23:57 -0500

    Implemented KL-NMF with multiplicative updates

---
+++ src/nmflib/nmf.py
@@ -0,0 +1,134 @@
+"""Nonnegative matrix factorisation code for count data."""
+# Copyright (C) 2019 Yilong Li (yilong.li.yl3@gmail.com) - All Rights Reserved
+
+
+import logging
+import numpy as np
+import sklearn.decomposition._nmf
+import time
+
+
+EPSILON = np.finfo(np.float32).eps
+
+
+def _mu_W(X, W, H):
+    """Multiplicative KL-update for W.
+
+    Args:
+        X (numpy.ndarray): A nonnegative integer matrix of shape (M, N).
+        W (numpy.ndarray): A nonnegative matrix of shape (M, K) for current W.
+        H (numpy.ndarray): A nonnegative matrix of shape (K, N) for current H.
+
+    Returns:
+        numpy.ndarray: The multiplicative update for W.
+    """
+
+    # Compute numerator.
+    numerator = np.dot(W, H)
+    numerator[numerator == 0] = EPSILON
+    np.divide(X, numerator, out=numerator)
+    numerator = np.dot(numerator, H.T)
+
+    # Compute denominator
+    H_sum = np.sum(H, axis=1)
+    denominator = H_sum[np.newaxis, :]
+    denominator[denominator == 0] = EPSILON
+
+    # Compute the update
+    numerator /= denominator
+    delta_W = numerator
+
+    return delta_W
+
+
+def _mu_H(X, W, H):
+    """Multiplicative KL-update for H.
+
+    Args:
+        X (numpy.ndarray): A nonnegative integer matrix of shape (M, N).
+        W (numpy.ndarray): A nonnegative matrix of shape (M, K) for current W.
+        H (numpy.ndarray): A nonnegative matrix of shape (K, N) for current H.
+
+    Returns:
+        numpy.ndarray: The multiplicative update for H.
+    """
+    # Compute numerator.
+    numerator = np.dot(W, H)
+    numerator[numerator == 0] = EPSILON
+    np.divide(X, numerator, out=numerator)
+    numerator = np.dot(W.T, numerator)
+
+    # Compute denominator
+    W_sum = np.sum(W, axis=0)
+    denominator = W_sum[:, np.newaxis]
+    denominator[denominator == 0] = EPSILON
+
+    # Compute the update
+    numerator /= denominator
+    delta_H = numerator
+
+    return delta_H
+
+
+def _kl_divergence(X, W, H):
+    """KL divergence for X ~ WH."""
+    WH = np.dot(W, H)
+    WH_raveled = WH.ravel()
+    X_raveled = X.ravel()
+
+    # Prevent the algorithm from crashing.
+    X_not_zero = X_raveled != 0
+    WH_raveled = WH_raveled[X_not_zero]
+    X_raveled = X_raveled[X_not_zero]
+    WH_raveled[WH_raveled == 0] = EPSILON
+
+    sum_WH = np.dot(np.sum(W, axis=0), np.sum(H, axis=1))
+    div = X_raveled / WH_raveled
+    res = np.dot(X_raveled, np.log(div))
+    res += sum_WH - np.sum(X_raveled)
+
+    return res
+
+
+def fit_nmf(X, k, max_iter=200, tol=1e-4, verbose=0, random_state=None):
+    """Fit KL-NMF using multiplicative updates."""
+    W, H = sklearn.decomposition._nmf._initialize_nmf(
+        X, k, 'random', random_state=random_state)
+
+    # Set up initial values.
+    start_time = time.time()
+    error_at_init = _kl_divergence(X, W, H)
+    previous_error = error_at_init
+    errors = []
+
+    for n_iter in range(1, max_iter + 1):
+        delta_W = _mu_W(X, W, H)
+        W *= delta_W
+
+        delta_H = _mu_H(X, W, H)
+        H *= delta_H
+
+        # Test for convergence every 10 iterations.
+        if n_iter % 10 == 0:
+            error = _kl_divergence(X, W, H)
+            errors.append(error)
+            if verbose:
+                elapsed = time.time() - start_time
+                logging.info(
+                    "Iteration %02d after %.3f seconds, error: %f".format(
+                        n_iter, elapsed, error))
+            if (previous_error - error) / error_at_init < tol:
+                break
+            previous_error = error
+
+    if verbose and n_iter % 10 != 0:
+        elapsed = time.time() - start_time
+        logging.info(
+            "Stopped after iteration %02d after %.3f seconds, error: %f".format(
+                n_iter, elapsed, error))
+
+    # Scale W and H such that W columns sum to 1.
+    W_colsums = np.sum(W, axis=0)
+    W /= W_colsums[np.newaxis, :]
+    H *= W_colsums[:, np.newaxis]
+    return W, H, n_iter, errors
---
+++ tests/test_nmf.py
@@ -0,0 +1,42 @@
+"""Test NMF code."""
+
+import numpy as np
+import nmflib.nmf
+
+
+class SimpleNMFData:
+    """Three channels and two signatures.
+
+    The first signature only creates mutations in the first channel. The second
+    signature creates mutations evenly in all channels.
+    """
+    X = np.array([[15, 40, 12, 85],
+                  [15, 20, 0,  25],
+                  [0,  10, 6,  30],
+                  [0,  20, 12, 60]])
+    W_true = np.array([[.5, 0.4],
+                       [.5, 0],
+                       [0,  0.2],
+                       [0,  0.4]])
+    H_true = np.array([[30, 40, 0,  50],
+                       [0,  50, 30, 150]])
+
+    def check_answer(W, H):
+        """Check if the fitted matrices are close to the true one."""
+        TOL = 1e-4
+        assert W.shape == (4, 2)
+        assert H.shape == (2, 4)
+
+        # Order columns in W to match W_true.
+        if W[0, 0] < W[0, 1]:
+            W = W[:, [1, 0]]
+            H = H[[1, 0], :]
+
+        assert np.all(np.abs(W - SimpleNMFData.W_true) < TOL)
+        assert np.all(np.abs(H - SimpleNMFData.H_true) < TOL)
+
+
+def test_fit_nmf():
+    W, H, n_iter, errors = nmflib.nmf.fit_nmf(
+        SimpleNMFData.X, 2, max_iter=10000, tol=1e-10)
+    SimpleNMFData.check_answer(W, H)
