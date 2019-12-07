0eda29be3d513a09946d6de41a6cf6915c4d5689
Yilong Li <yilong.li.yl3@gmail.com>
2019-12-06 22:38:39 -0500

    Minor change

--- src/mutlib/utils.py
+++ src/mutlib/utils.py
@@ -1,10 +1,10 @@
 """General purpose functions for point mutations."""
 
-COMPLEMENT = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
+import mutlib
 
 
 def revcomp(seq):
     """Reverse complement a DNA string sequence."""
     seq_rev = seq[::-1]
-    seq_revcomp = "".join([COMPLEMENT[nt] for nt in seq_rev])
+    seq_revcomp = "".join([mutlib.COMPLEMENT[nt] for nt in seq_rev])
     return seq_revcomp
