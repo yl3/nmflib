98018628874e0abe6a26347d2b088e7676b280c1
Yilong Li <yilong.li.yl3@gmail.com>
2019-12-06 22:40:22 -0500

    Functions for computing mutation context matrix

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,7 @@
 """Test mutlib.utils."""
 
 import pytest
+import pandas as pd
 
 import mutlib
 import mutlib.contexts
@@ -18,17 +19,107 @@ def test_get_context(datafiles):
     context_finder = mutlib.contexts.ContextFinder(fasta_file)
 
     # Flipped context
-    assert context_finder.get_unnormalised_context('1', 10002, 3) \
-        == ('TAA', 'A')
-    assert context_finder.get_context('1', 10002, 3) == ('TTA', 'T')
+    assert context_finder.get_unnormalised_context('1', 10002, 3) == 'TAA'
+    assert context_finder.get_context('1', 10002, 3) == ('TTA', True)
 
     # Unflipped context
-    assert context_finder.get_unnormalised_context('1', 10004, 5) \
-        == ('AACCC', 'C')
-    assert context_finder.get_context('1', 10004, 5) == ('AACCC', 'C')
+    assert context_finder.get_unnormalised_context('1', 10004, 5) == 'AACCC'
+    assert context_finder.get_context('1', 10004, 5) \
+        == ('AACCC', False)
 
     # Context length must be an odd number.
     with pytest.raises(ValueError):
         context_finder.get_context('1', 10004, 2)
     with pytest.raises(ValueError):
         context_finder.get_context('1', 10004, 4)
+
+    # Test the context array
+    context_df = context_finder.get_context_array(['1', '1'], [10002, 10004])
+    assert isinstance(context_df, pd.DataFrame)
+    assert (list(context_df.itertuples(index=False, name=None))
+            == [('TTA', True), ('ACC', False)])
+
+    # Test if the Pandas index is passed over properly.
+    context_df = context_finder.get_context_array(['1', '1'], [10002, 10004])
+    assert (context_df.index == ['1:10002', '1:10004']).all()
+
+    context_df = context_finder.get_context_array(
+        pd.Series(['1', '1'], index=['test1', 'test2']),
+        [10002, 10004])
+    assert (context_df.index == ['test1', 'test2']).all()
+
+
+def test_count_snv_types(datafiles):
+    contexts = pd.DataFrame(
+        [('ATT', True),
+         ('ATT', True),
+         ('GCC', False)],
+        columns=['context', 'flipped']
+    )
+    ref = ['A', 'A', 'C']
+    alt = ['T', 'T', 'T']
+    samples = ['sample_1', 'sample_1', 'sample_2']
+
+    # Check that ref must be different to alt.
+    with pytest.raises(ValueError):
+        bad_alt = ['A', 'T', 'C']  # First alt is the same as first ref.
+        mutlib.contexts.count_snv_types(contexts, ref, bad_alt, samples, 3)
+
+    # Correct ref, no problem
+    mut_counts = mutlib.contexts.count_snv_types(contexts, ref, alt, samples, 3)
+    expected_index = pd.DataFrame([('C>T', 'GCC'),
+                                   ('T>A', 'ATT')])
+    assert (mut_counts.index.to_frame() == expected_index.values).all(None)
+    assert (mut_counts.columns == ['sample_1', 'sample_2']).all()
+    expected_values = pd.DataFrame(
+        [(0, 1),
+         (2, 0)]
+    )
+    assert (mut_counts == expected_values.values).all(None)
+
+    # If the (first) reference doesn't match that of the context, the respective
+    # mutations should be ignored.
+    expected_values = pd.DataFrame(
+        [(0, 1),
+         (1, 0)]
+    )
+    bad_ref = ['C', 'A', 'C']  # ref mismatch with the "reference"
+    mut_counts = mutlib.contexts.count_snv_types(contexts, bad_ref, alt,
+                                                 samples, 3)
+    assert (mut_counts == expected_values.values).all(None)
+
+    # A context that contains N's should be ignored.
+    bad_contexts = pd.DataFrame(
+        [('NTT', True),
+         ('ATT', True),
+         ('GCC', False)],
+        columns=['context', 'flipped']
+    )
+    mut_counts = mutlib.contexts.count_snv_types(bad_contexts, ref, alt,
+                                                 samples, 3)
+    expected_index = pd.DataFrame([('C>T', 'GCC'),
+                                   ('T>A', 'ATT')])
+    assert (mut_counts.index.to_frame() == expected_index.values).all(None)
+    assert (mut_counts.columns == ['sample_1', 'sample_2']).all()
+    expected_values = pd.DataFrame([(0, 1),
+                                    (1, 0)])
+    assert (mut_counts == expected_values.values).all(None)
+
+
+@pytest.mark.datafiles('test_data/hs37d5.1:1-100000.fa.gz')
+def test_compute_and_count_snv_types(datafiles):
+    fasta_file = str(datafiles) + '/hs37d5.1:1-100000.fa.gz'
+    chroms = ['1', '1', '1']
+    pos = [10002, 10004, 10002]
+    ref = ['A', 'C', 'A']
+    alt = ['T', 'G', 'T']
+    samples = ['sample_1', 'sample_1', 'sample_2']
+    mut_counts = mutlib.contexts.compute_and_count_snv_types(
+        fasta_file, chroms, pos, ref, alt, samples, 3)
+    expected_index = pd.DataFrame([('C>G', 'ACC'),
+                                   ('T>A', 'TTA')])
+    assert (mut_counts.index.to_frame() == expected_index.values).all(None)
+    assert (mut_counts.columns == ['sample_1', 'sample_2']).all()
+    expected_values = pd.DataFrame([(1, 0),
+                                    (1, 1)])
+    assert (mut_counts == expected_values.values).all(None)
