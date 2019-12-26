b208fd5256a839de07b628bbeb4fb8972fe1bc4f
Yilong Li <yilong.li.yl3@gmail.com>
2019-12-25 23:42:05 -0500

    Add multiprocessing.get_context("spawn")
    Helps numpy work, because numpy uses multiple threads? Refer to
    https://codewithoutrules.com/2018/09/04/python-multiprocessing/.

--- src/nmflib/nmf.py
+++ src/nmflib/nmf.py
@@ -512,7 +512,7 @@ def gof(X, X_exp, sim_count=None, random_state=None, r=None, n_processes=1):
         sim_logliks = _sim_loglik_helper_func((X, sim_count, X_exp, r, p))
         sim_logliks = np.array(sim_logliks)
     else:
-        process_pool = multiprocessing.Pool(n_processes)
+        process_pool = multiprocessing.get_context("spawn").Pool(n_processes)
         args_list = [(X, sim_count, X_exp, r, p)] * n_processes
         sim_logliks = process_pool.imap_unordered(_sim_loglik_helper_func,
                                                   args_list)
@@ -687,8 +687,8 @@ def mpfit(
         args_list = []
         for i in range(random_inits):
             args_list.append((X, k, S, O, nbinom_fit, nb_fit_freq_base,
-                              max_iter, abstol, False, random_state + i, True))
-    process_pool = multiprocessing.Pool(n_processes)
+                              max_iter, abstol, False, random_state + i))
+    process_pool = multiprocessing.get_context("spawn").Pool(n_processes)
     if verbose:
         fitted_res = list(
             tqdm.tqdm(process_pool.imap_unordered(unpack_args_fit, args_list),
